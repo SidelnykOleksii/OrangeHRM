adab85adccb3a6525525f6df9dd9761b135c03a1
oleksii_laptop <formovka4@ukr.net>
2024-09-03 14:21:30 +0300

    refactor locators

--- PageObjects/AdminPage/job.py
+++ PageObjects/AdminPage/job.py
@@ -4,29 +4,29 @@ from Data.assertions import Assertions
 from Data.variables import PathToTestFiles, PageUrls
 
 
-# buttons
-ADD_JOB_TITLE = "//div[@class='orangehrm-header-container']//button"
-
-# text fields
-JOB_TITLE_FIELD = "form > div:nth-child(1) > div > div:nth-child(2) > input"
-JOB_DESCRIPTION_FIELD = "form > div:nth-child(2) > div > div:nth-child(2) > textarea"
-
-# other
-JOB_SPECIFICATION_INPUT = "//div[@class='oxd-file-input-div']"
-
-
 class JobPage(Base):
     def __init__(self, page: Page) -> None:
         super().__init__(page)
         self.assertions = Assertions(page)
 
+    # locators
+    # buttons
+    ADD_JOB_TITLE = "//div[@class='orangehrm-header-container']//button"
+
+    # text fields
+    JOB_TITLE_FIELD = "form > div:nth-child(1) > div > div:nth-child(2) > input"
+    JOB_DESCRIPTION_FIELD = "form > div:nth-child(2) > div > div:nth-child(2) > textarea"
+
+    # other
+    JOB_SPECIFICATION_INPUT = "//div[@class='oxd-file-input-div']"
+
     def add_new_job_titles(self, job_title: str, job_desc: str, note: str, file_name: str):
         self.select_dropdown_option_by_name(dropdown="Job ", option_text="Job Titles")
-        self.page.locator(ADD_JOB_TITLE).click()
-        self.input(JOB_TITLE_FIELD, job_title)
-        self.input(JOB_DESCRIPTION_FIELD, job_desc)
-        self.upload_file(locator=JOB_SPECIFICATION_INPUT, file_path=PathToTestFiles.upload_file_valid())
-        loc = self.page.locator(JOB_SPECIFICATION_INPUT)
+        self.page.locator(self.ADD_JOB_TITLE).click()
+        self.input(self.JOB_TITLE_FIELD, job_title)
+        self.input(self.JOB_DESCRIPTION_FIELD, job_desc)
+        self.upload_file(locator=self.JOB_SPECIFICATION_INPUT, file_path=PathToTestFiles.upload_file_valid())
+        loc = self.page.locator(self.JOB_SPECIFICATION_INPUT)
         expect(loc).to_have_text(file_name)
         add_note_loc = self.page.get_by_placeholder(text="Add note")
         add_note_loc.fill(note)
--- PageObjects/default_page.py
+++ PageObjects/default_page.py
@@ -4,24 +4,6 @@ from PageObjects.base import Base
 from Data.assertions import Assertions
 import logging
 
-# user details header
-USER_DETAILS_DROPDOWN = '//span[@class="oxd-userdropdown-tab"]'
-USER_DETAILS_DROPDOWN_MENU = '//ul[@class="oxd-dropdown-menu"]'
-HEADER_LEFT_SIDE_TEXT = "//div[@class='oxd-topbar-header']//span/h6"
-
-# Left side menu
-LEFT_SIDE_MENU_SEARCH_FIELD = "//input[@placeholder='Search']"
-LEFT_SIDE_MENU_ITEMS = "//ul[@class='oxd-main-menu']/li"
-LEFT_SIDE_MENU_ITEM = "//ul[@class='oxd-main-menu']/li//span"
-
-# About pop-up
-ABOUT_POP_UP = '//div[@class="oxd-sheet oxd-sheet--rounded oxd-sheet--white oxd-dialog-sheet oxd-dialog-sheet--' \
-               'shadow oxd-dialog-sheet--gutters"]'
-ABOUT_POP_UP_TITLE_ITEMS = '//div//p[@class="oxd-text oxd-text--p orangehrm-about-title"]'
-ABOUT_POP_UP_CLOSE_BUTTON = "//button[@class='oxd-dialog-close-button oxd-dialog-close-button-position']"
-
-about_pop_up_expected_items = ['Company Name:', 'Version:', 'Active Employees:', 'Employees Terminated:']
-
 
 class DefaultPageObjects(Base):
     def __init__(self, page: Page) -> None:
@@ -30,49 +12,68 @@ class DefaultPageObjects(Base):
         logging.basicConfig(filename='logging.log', level=logging.INFO)
         self.logger = logging.getLogger(__name__)
 
+    # locators
+    # user details header
+    USER_DETAILS_DROPDOWN = '//span[@class="oxd-userdropdown-tab"]'
+    USER_DETAILS_DROPDOWN_MENU = '//ul[@class="oxd-dropdown-menu"]'
+    HEADER_LEFT_SIDE_TEXT = "//div[@class='oxd-topbar-header']//span/h6"
+
+    # Left side menu
+    LEFT_SIDE_MENU_SEARCH_FIELD = "//input[@placeholder='Search']"
+    LEFT_SIDE_MENU_ITEMS = "//ul[@class='oxd-main-menu']/li"
+    LEFT_SIDE_MENU_ITEM = "//ul[@class='oxd-main-menu']/li//span"
+
+    # About pop-up
+    ABOUT_POP_UP = '//div[@class="oxd-sheet oxd-sheet--rounded oxd-sheet--white oxd-dialog-sheet oxd-dialog-sheet--' \
+                   'shadow oxd-dialog-sheet--gutters"]'
+    ABOUT_POP_UP_TITLE_ITEMS = '//div//p[@class="oxd-text oxd-text--p orangehrm-about-title"]'
+    ABOUT_POP_UP_CLOSE_BUTTON = "//button[@class='oxd-dialog-close-button oxd-dialog-close-button-position']"
+
+    about_pop_up_expected_items = ['Company Name:', 'Version:', 'Active Employees:', 'Employees Terminated:']
+
     @allure.step
     def click_log_out_button(self):
-        self.click(USER_DETAILS_DROPDOWN)
+        self.click(self.USER_DETAILS_DROPDOWN)
         self.page.get_by_role("menuitem", name="Logout").click()
 
     def click_about_button(self):
-        self.click(USER_DETAILS_DROPDOWN)
-        self.page.wait_for_selector(USER_DETAILS_DROPDOWN_MENU)
+        self.click(self.USER_DETAILS_DROPDOWN)
+        self.page.wait_for_selector(self.USER_DETAILS_DROPDOWN_MENU)
         self.page.get_by_role("menuitem", name="About").click()
 
     def left_side_menu_search(self, input_value: str, expected_count: int):
         self.page.wait_for_load_state('load')
-        self.input(LEFT_SIDE_MENU_SEARCH_FIELD, input_value)
-        actual_count = self.page.locator(LEFT_SIDE_MENU_ITEMS).count()
+        self.input(self.LEFT_SIDE_MENU_SEARCH_FIELD, input_value)
+        actual_count = self.page.locator(self.LEFT_SIDE_MENU_ITEMS).count()
         assert actual_count == expected_count
 
     def get_default_left_side_menu_items(self):
-        self.page.wait_for_selector(LEFT_SIDE_MENU_ITEMS)
-        count = self.page.locator(LEFT_SIDE_MENU_ITEMS).count()
+        self.page.wait_for_selector(self.LEFT_SIDE_MENU_ITEMS)
+        count = self.page.locator(self.LEFT_SIDE_MENU_ITEMS).count()
         assert count == 12
 
     def select_left_side_menu_item(self, item_name: str):
-        self.page.wait_for_selector(LEFT_SIDE_MENU_ITEMS)
+        self.page.wait_for_selector(self.LEFT_SIDE_MENU_ITEMS)
         self.page.get_by_role("link", name=item_name).click()
 
     def log_out(self):
-        if self.page.is_visible(USER_DETAILS_DROPDOWN):
+        if self.page.is_visible(self.USER_DETAILS_DROPDOWN):
             self.click_log_out_button()
         else:
             pass
 
     @allure.step
     def assert_title_items_in_about_pop_up(self):
-        expected = sorted(about_pop_up_expected_items)
+        expected = sorted(self.about_pop_up_expected_items)
         self.click_about_button()
-        self.page.wait_for_selector(ABOUT_POP_UP_TITLE_ITEMS)
-        actual = self.get_items_lists(ABOUT_POP_UP_TITLE_ITEMS)
+        self.page.wait_for_selector(self.ABOUT_POP_UP_TITLE_ITEMS)
+        actual = self.get_items_lists(self.ABOUT_POP_UP_TITLE_ITEMS)
         self.logger.info(f"Expected items: {expected}")
         self.logger.info(f"Actual items: {actual}")
         for item in expected:
             assert item in actual, f"Item '{item}' is missing in the actual list of items"
             self.logger.info(f"Item '{item}' is present in the actual list of items")
-        self.page.locator(ABOUT_POP_UP_CLOSE_BUTTON).click()
+        self.page.locator(self.ABOUT_POP_UP_CLOSE_BUTTON).click()
 
     def assert_header_item_according_to_selected_menu_item(self):
         menu_items = [
@@ -80,6 +81,6 @@ class DefaultPageObjects(Base):
             ("Directory", "Directory"),
         ]
         for menu_text, expected_header_text in menu_items:
-            self.page.locator(LEFT_SIDE_MENU_ITEMS, has_text=menu_text).click()
-            actual_header_text = self.page.locator(HEADER_LEFT_SIDE_TEXT).inner_text()
+            self.page.locator(self.LEFT_SIDE_MENU_ITEMS, has_text=menu_text).click()
+            actual_header_text = self.page.locator(self.HEADER_LEFT_SIDE_TEXT).inner_text()
             assert actual_header_text == expected_header_text
