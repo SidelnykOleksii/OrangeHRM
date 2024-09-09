e70a6bd3d3f6fab1a8482ea2fbd5db16b4557a4d
oleksii_laptop <formovka4@ukr.net>
2024-09-09 10:12:37 +0300

    added temporary wait refactoring

--- PageObjects/login.py
+++ PageObjects/login.py
@@ -14,26 +14,33 @@ class LoginPage(Base):
     USERNAME_FIELD = "//input[@name='username']"
     PASSWORD_FIELD = "//input[@name='password']"
     SUBMIT_BUTTON = "//button[@type='submit']"
-    INVALID_CREDENTIALS_ERROR_MESSAGE = "//p[text()[contains(.,'Invalid credentials')]]"
-    REQUIRED_ERROR_MESSAGE = "//div/span[@class='oxd-text oxd-text--span oxd-input-field-error-message" \
-                             " oxd-input-group__message']"
+    INVALID_CREDENTIALS_ERROR_MESSAGE = "//div[contains(@class, 'oxd-alert-content--error')]" \
+                                        "//p[text()='Invalid credentials']"
+    REQUIRED_ERROR_MESSAGE = "//span[contains(@class, 'input-field-error-message oxd') and text()='Required']"
 
     @allure.step
     def user_login(self):
+        # TODO: think about solution for not use wait_for_timeout.
         self.open("web/index.php/auth/login")
         self.input(self.USERNAME_FIELD, Constants.login)
         self.input(self.PASSWORD_FIELD, Constants.password)
         self.click(self.SUBMIT_BUTTON)
         self.assertions.check_url("https://opensource-demo.orangehrmlive.com/web/index.php/dashboard/index",
                                   "Wrong URL")
+        self.page.wait_for_timeout(timeout=1000)  # temporary solution, without this check test is passed
+        # if set expect(loc).to_be_hidden for assert_header_is_visible()
         self.assert_header_is_visible()
 
     @allure.step
     def login_invalid_data(self, username: str, password: str):
+        # TODO: think about solution for not use wait_for_timeout.
         self.open("web/index.php/auth/login")
         self.input(self.USERNAME_FIELD, username)
         self.input(self.PASSWORD_FIELD, password)
         self.click(self.SUBMIT_BUTTON)
+        self.page.wait_for_load_state('domcontentloaded')
+        self.page.wait_for_timeout(timeout=1000)  # temporary solution, without this check test is passed
+        # if set check_absence
         self.assertions.check_presence(self.INVALID_CREDENTIALS_ERROR_MESSAGE)
 
     def login_empty_fields(self, username: str, password: str):
