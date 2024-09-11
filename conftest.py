570bc3cebbc3eb8e13b101f9ae5825628b4c6080
oleksii_laptop <formovka4@ukr.net>
2024-09-11 14:47:54 +0300

    updated pytest_addoption

--- conftest.py
+++ conftest.py
@@ -16,7 +16,6 @@ def pytest_addoption(parser):
     parser.addoption('--bn', action='store', default="chrome", help="Choose browser: chrome, remote_chrome or firefox")
     parser.addoption('--h', action='store', default=True, help='Choose headless: True or False')
     parser.addoption('--s', action='store', default={'width': 1920, 'height': 1080}, help='Size window: width,height')
-    parser.addoption('--slow', action='store', default=200, help='Choose slow_mo for robot action')
+    parser.addoption('--slow', action='store', default=200, help='Choose slow_mo for actions in the browser')
     parser.addoption('--t', action='store', default=10000, help='Choose timeout')
     parser.addoption('--l', action='store', default='en_GB', help='Choose locale')
-    parser.addoption('--env', action='store', default="local", help="Environment (local or ci)")
